3c331a2fa6b504246b2b2384fef6b6f068cf7392
christophe <tremblay717@gmail.com>
2022-10-08 15:44:54 -0400

    export object display funciton in projects.js

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import './style.css';
 import UserIcon from './utilisateur.png';
-// import projects from './projects.js';
+import projects from './projects.js';
 
 //Top bar header
 const header = document.createElement('div');
@@ -35,83 +35,27 @@ document.body.appendChild(mainSection);
 
 //1st part is a left bar
 const leftBar = document.createElement('div');
+leftBar.id = 'leftBar';
 leftBar.className = 'leftBar';
 mainSection.appendChild(leftBar);
 
-const projectDiv = document.createElement('div');
-projectDiv.className = 'projectDiv';
-leftBar.appendChild(projectDiv);
-
-// 2nd part is a div taking 75% of the page's width
-const todoSection = document.createElement('div');
-todoSection.id = 'todoSection';
-mainSection.appendChild(todoSection);
-
-//Span tag for my project list
-const myProjects = document.createElement('span');
-myProjects.className = 'myProjects';
-myProjects.textContent = 'My Projects';
-projectDiv.appendChild(myProjects);
-
-
-
-function CreateProject(title, description, dueDate, priority) {
+function Project(title, description, dueDate, priority) {
 
     this.title = title;
     this.description = description;
     this.dueDate = dueDate;
     this.priority = priority;
-
-    const projectTitle = document.createElement('span');
-    projectTitle.className = 'projectTitle'
-    projectTitle.textContent = this.title;
-
-    projectDiv.appendChild(projectTitle);
-
-
-    const box = document.createElement('box');
-    box.className = 'projectBox';
-    box.id = this.title;
-
-
-    const boxTitle = document.createElement('span');
-    boxTitle.textContent = "Title: " + this.title;
-
-    const boxDescription = document.createElement('span');
-    boxDescription.textContent = "Description: " + this.description;
-
-
-    const boxDate = document.createElement('span');
-    boxDate.textContent = "Due date: " + this.dueDate;
-
-    const boxPriority = document.createElement('span');
-    boxPriority.textContent = "Priority: " + this.priority;
-
-    const boxContent = [boxTitle, boxDescription, boxDate, boxPriority];
-
-    for (let i = 0; i < boxContent.length; i++) {
-        box.appendChild(boxContent[i]);
-    }
-
-
-    todoSection.appendChild(box)
-
-
-
-
-
-
 }
 
+const home = new Project('Home', 'Renovating my Home', '2023-12-31', 'Low');
 
-const home = new CreateProject('Home', 'Renovating my Home', '2023-12-31', 'Low');
+const school = new Project('School', 'Finishing my degree', '2023-12-31', 'Low');
 
-const school = new CreateProject('School', 'Finishing my degree', '2023-12-31', 'Low');
+const groceries = new Project('Groceries', 'Buy my groceries', '2023-01-01', 'High');
 
-const groceries = new CreateProject('Groceries', 'Buy my groceries', '2023-01-01', 'High');
+const car = new Project('New Car', 'Buy a new car', '2023-12-31', 'Medium');
 
-const car = new CreateProject('New Car', 'Buy a new car', '2023-12-31', 'Medium');
+let projectList = [home, school, groceries, car];
 
+projects(projectList);
 
-
-// Main Section
--- src/projects.js
+++ src/projects.js
@@ -1,12 +1,54 @@
-export default function projects() {
+export default function projects(projectList) {
 
-   function CreateProject(title, description, dueDate, priority) {
+    const mainSection = document.getElementById('mainSection');
 
-        this.title = title;
-        this.description = description;
-        this.dueDate = dueDate;
-        this.priority = priority;
+    //1st part is a left bar
+    const leftBar = document.getElementById('leftBar');
+
+    const projectDiv = document.createElement('div');
+    projectDiv.className = 'projectDiv';
+    leftBar.appendChild(projectDiv);
+
+    // 2nd part is a div taking 75% of the page's width
+    const todoSection = document.createElement('div');
+    todoSection.id = 'todoSection';
+    mainSection.appendChild(todoSection);
+
+    //Span tag for my project list
+    const myProjects = document.createElement('span');
+    myProjects.className = 'myProjects';
+    myProjects.textContent = 'My Projects';
+    projectDiv.appendChild(myProjects);
+
+    for (let i = 0; i < projectList.length; i++) {
+
+        const projectTitle = document.createElement('span');
+        projectTitle.className = 'projectTitle'
+        projectTitle.textContent = projectList[i].title
+
+        projectDiv.appendChild(projectTitle);
+
+        const box = document.createElement('box');
+        box.className = 'projectBox';
+        box.id = projectList[i].title
+
+        const boxTitle = document.createElement('span');
+        boxTitle.textContent = "Title: " + projectList[i].title;
+
+        const boxDescription = document.createElement('span');
+        boxDescription.textContent = "Description: " + projectList[i].description;
+
+        const boxDate = document.createElement('span');
+        boxDate.textContent = "Due date: " + projectList[i].dueDate;
+
+        const boxPriority = document.createElement('span');
+        boxPriority.textContent = "Priority: " + projectList[i].priority;
+
+        const boxContent = [boxTitle, boxDescription, boxDate, boxPriority];
+
+        for (let i = 0; i < boxContent.length; i++) {
+            box.appendChild(boxContent[i]);
+        }
+        todoSection.appendChild(box)
     }
-    
-    return CreateProject;
 }
