8c5a1dccb0c77547dff9cc73a315afdf81ab0b53
christophe <tremblay717@gmail.com>
2022-10-08 14:12:16 -0400

    added objects by default

--- src/index.js
+++ src/index.js
@@ -1,4 +1,5 @@
 import './style.css';
+// import projects from './projects.js';
 
 //Top bar header
 const header = document.createElement('div');
@@ -25,9 +26,42 @@ const projectDiv = document.createElement('div');
 projectDiv.className = 'projectDiv';
 leftBar.appendChild(projectDiv);
 
-// 2nd part is as div taking 75% of the page's width
+// 2nd part is a div taking 75% of the page's width
 const todoSection = document.createElement('div');
+todoSection.id = 'todoSection';
 todoSection.style.backgroundColor = '#FFEC89';
 todoSection.style.width = '100%';
 todoSection.style.minHeight = '100vh'
 mainSection.appendChild(todoSection);
+
+//Span tag for my project list
+const myProjects = document.createElement('span');
+myProjects.className = 'myProjects';
+myProjects.textContent = 'My Projects';
+projectDiv.appendChild(myProjects);
+
+
+
+function CreateProject(title, description, dueDate, priority) {
+
+    this.title = title;
+    this.description = description;
+    this.dueDate = dueDate;
+    this.priority = priority;
+
+    const projectTitle = document.createElement('span');
+    projectTitle.className = 'projectTitle'
+    projectTitle.textContent = this.title;
+
+    projectDiv.appendChild(projectTitle);
+
+}
+
+
+const home = new CreateProject('Home', 'Renovating my Home', '2023-12-31', 'Low');
+
+const school = new CreateProject('School', 'Finishing my degree', '2023-12-31', 'Low');
+
+const groceries = new CreateProject('Groceries','Buy my groceries','2023-01-01','High');
+
+const car = new CreateProject('New Car','Buy a new car','2023-12-31','Medium');
---
+++ src/projects.js
@@ -0,0 +1,12 @@
+export default function projects() {
+
+   function CreateProject(title, description, dueDate, priority) {
+
+        this.title = title;
+        this.description = description;
+        this.dueDate = dueDate;
+        this.priority = priority;
+    }
+    
+    return CreateProject;
+}
