65bfe05de942b125eac6ac7964809cbd28d34a08
christophe <tremblay717@gmail.com>
2022-10-08 15:55:09 -0400

    html stuff sent to template.js

--- src/index.js
+++ src/index.js
@@ -1,43 +1,8 @@
 import './style.css';
-import UserIcon from './utilisateur.png';
+import basicHtml from './template.js';
 import projects from './projects.js';
 
-//Top bar header
-const header = document.createElement('div');
-header.className = 'header';
-document.body.appendChild(header);
-
-// Page Title
-const hero = document.createElement('span');
-hero.className = 'hero';
-hero.textContent = 'Todo List';
-header.appendChild(hero);
-
-//Right Part - Header
-const rightHeaderDiv = document.createElement('div');
-rightHeaderDiv.className = 'righHeaderDiv';
-header.appendChild(rightHeaderDiv);
-
-const userIcon = new Image();
-userIcon.id = 'userIcon';
-userIcon.src = UserIcon;
-rightHeaderDiv.appendChild(userIcon)
-
-const userName = document.createElement('span');
-userName.className = 'userName';
-userName.textContent = '@guestUser';
-rightHeaderDiv.appendChild(userName);
-
-// Splitting the rest of the page in two sections
-const mainSection = document.createElement('div');
-mainSection.id = 'mainSection';
-document.body.appendChild(mainSection);
-
-//1st part is a left bar
-const leftBar = document.createElement('div');
-leftBar.id = 'leftBar';
-leftBar.className = 'leftBar';
-mainSection.appendChild(leftBar);
+basicHtml(); // Displaying the basic html template through a function
 
 function Project(title, description, dueDate, priority) {
 
@@ -47,15 +12,13 @@ function Project(title, description, dueDate, priority) {
     this.priority = priority;
 }
 
+//Creating a few random projects to complete
 const home = new Project('Home', 'Renovating my Home', '2023-12-31', 'Low');
-
 const school = new Project('School', 'Finishing my degree', '2023-12-31', 'Low');
-
 const groceries = new Project('Groceries', 'Buy my groceries', '2023-01-01', 'High');
-
 const car = new Project('New Car', 'Buy a new car', '2023-12-31', 'Medium');
 
-let projectList = [home, school, groceries, car];
+let projectList = [home, school, groceries, car]; //Objects are stored in this array, so we can update our display at any time when calling specific functions
 
+//Displaying our objects on screen
 projects(projectList);
-
---
+++ src/template.js
@@ -0,0 +1,42 @@
+import UserIcon from './utilisateur.png';
+
+export default function basicHtml() {
+
+    //Top bar header
+    const header = document.createElement('div');
+    header.className = 'header';
+    document.body.appendChild(header);
+
+    // Page Title
+    const hero = document.createElement('span');
+    hero.className = 'hero';
+    hero.textContent = 'Todo List';
+    header.appendChild(hero);
+
+    //Right Part - Header
+    const rightHeaderDiv = document.createElement('div');
+    rightHeaderDiv.className = 'righHeaderDiv';
+    header.appendChild(rightHeaderDiv);
+
+    const userIcon = new Image();
+    userIcon.id = 'userIcon';
+    userIcon.src = UserIcon;
+    rightHeaderDiv.appendChild(userIcon)
+
+    const userName = document.createElement('span');
+    userName.className = 'userName';
+    userName.textContent = '@guestUser';
+    rightHeaderDiv.appendChild(userName);
+
+    // Splitting the rest of the page in two sections
+    const mainSection = document.createElement('div');
+    mainSection.id = 'mainSection';
+    document.body.appendChild(mainSection);
+
+    //1st part is a left bar
+    const leftBar = document.createElement('div');
+    leftBar.id = 'leftBar';
+    leftBar.className = 'leftBar';
+    mainSection.appendChild(leftBar);
+
+}
