770d848a078207eed6cc67be604191284fe33c27
christophe <tremblay717@gmail.com>
2022-10-10 16:54:12 -0400

    cleaned up dist folder

--- webpack.config.js
+++ webpack.config.js
@@ -17,6 +17,7 @@ module.exports = {
     output: {
         filename: '[name].bundle.js',
         path: path.resolve(__dirname, 'dist'),
+        clean: true,
     },
     module: {
         rules: [{
@@ -28,7 +29,6 @@ module.exports = {
                 type: 'asset/resource',
             },
         ],
-
     },
     optimization: {
         runtimeChunk: 'single',
